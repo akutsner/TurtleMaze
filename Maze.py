a069f8f40fa10d104df118eea6699a9c6403689c
akutsner <akutsner@ithaca.edu>
2025-11-12 12:39:47 -0500

    crreated the background color and the turtle drawing color and scale

--- Maze.py
+++ Maze.py
@@ -22,41 +22,20 @@ for counter in range(total):
     grid_list.append(counter)
     visit.append(0)
     visited.append(0)
+def tset():
+    global t, wn
+    wn = turtle.Screen()
+    wn.bgcolor("black")
+    t = turtle.Turtle()
 
-# --- Maze directions (NESW) ---
-def NESW(current):
-    global strange
-    cords = [current + rows, current + 1, current - rows, current - 1]
-    stats = []
+    t.speed(3)
+    t.pensize(int(scale * 0.7))
+    t.color("grey")
 
-    for i in cords:
-        if i < 0 or i >= total or (i // rows != current // rows and i % rows != current % rows):
-            stats.append(3)  # impossible move
-        else:
-            stats.append(visit[i])
-
-    if visit[current] <= min(stats):
-        return visited[current]
-
-    min_count = stats.count(min(stats))
-    options = [1, 3]
-    choice = options[random.randint(0, 1)]
-
-    if strange and min_count == choice:
-        return visited[current]
-
-    NESW_choice = random.randint(0, min_count - 1)
-    counter = 0
-    for idx, val in enumerate(stats):
-        if val == min(stats):
-            if counter == NESW_choice:
-                visit[cords[idx]] += 1
-                visited[cords[idx]] = current
-                return cords[idx]
-            counter += 1
 
 
 def main():
+    tset()
 
 main()
 
